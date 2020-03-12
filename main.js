b06f597204144c83051746af03247088e2935f61
ehall0 <47374812+ehall0@users.noreply.github.com>
2020-03-11 19:57:03 -0500

    Add files via upload
    created game logic

---
+++ main.js
@@ -0,0 +1,51 @@
+const cards=[
+{
+rank: "queen",
+suit: "hearts",
+cardImage: 	"images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: 	"images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png"
+}];
+const cardsInPlay =[];
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
+}
+function flipCard(){
+let cardId = this.getAttribute('data-id');
+ this.src = cards[cardId].cardImage;
+console.log("User flipped "+ cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+}
+
+checkForMatch();
+
+function createBoard(){
+	for(let i=0; i < cards.length; i++){
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src','images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click',flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
